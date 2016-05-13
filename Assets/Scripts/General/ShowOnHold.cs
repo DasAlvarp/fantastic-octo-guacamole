d7d0985e662fd54839d94d16d10f26111e7cf255
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-05-13 15:56:19 -0400

    missed a spot

---
+++ Assets/Scripts/General/ShowOnHold.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowOnHold : MonoBehaviour {
+
+    public string buttonName;
+    public GameObject toToggle;
+	// Use this for initialization
+	void Start () {
+        toToggle.active = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown(buttonName))
+        {
+            toToggle.active = true;
+        }
+        if(Input.GetButtonUp(buttonName))
+        {
+            toToggle.active = false;
+        }
+	
+	}
+}
