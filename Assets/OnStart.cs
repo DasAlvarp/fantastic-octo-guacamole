aad4866db1b9132c9c7e4ee049e4306ecf25fc4c
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-05-17 14:11:43 -0400

    Starting on loading. It half works now

---
+++ Assets/OnStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Saver>().Load();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
