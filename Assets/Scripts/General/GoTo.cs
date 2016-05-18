dfd312d992b8cdec54621505d9d3e1ab8de15fe9
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-05-18 13:27:40 -0400

    New build. Some menu work too.

---
+++ Assets/Scripts/General/GoTo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoTo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GoToStage(string level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
