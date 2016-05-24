355b9aa61345462993a36ac2f8865a2ee2d2efc0
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-05-24 13:01:06 -0400

    Woo levers

---
+++ Assets/Scripts/InGame/ChangeFace.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ChangeFace : MonoBehaviour {
+    public Material offSt;
+    public Material onSt;
+    bool state;
+
+    void OnStart()
+    {
+        state = true;
+    }
+    public bool Toggle()
+    {       
+        if (state)
+        {
+            gameObject.GetComponent<Renderer>().material = offSt;
+            state = false;
+            return true;
+        }
+        gameObject.GetComponent<Renderer>().material = onSt;
+        state = true;
+        return false;
+ 
+    }
+}
---
+++ Assets/Scripts/InGame/OnToggle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class OnToggle : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        ChangeFace[] things  = transform.GetComponentsInChildren<ChangeFace>();
+        for(int x = 0; x < things.Length; x++)
+        {
+            things[x].Toggle();
+        }
+    }
+}
