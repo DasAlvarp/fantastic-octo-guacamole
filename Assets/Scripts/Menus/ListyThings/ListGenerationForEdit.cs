0a0a4ded7b5c1a25d564a6dc244d741d6ff4e9d3
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-05-26 12:28:06 -0400

    Improving UI, folders

---
+++ Assets/Scripts/Menus/ListyThings/ListGenerationForEdit.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System.Collections;
+
+public class ListGenerationForEdit : MonoBehaviour
+{
+
+    public Text itemList;
+    public Text selector;
+    public Text selectedItem;
+    float time;
+    ArrayList options;
+    int selected;
+
+    //sets up dropdown menus
+
+    // Use this for initialization
+    void Awake()
+    {
+        //coulda been OnStart.
+        options = new ArrayList();
+        selected = 0;
+        PopulateOptions();
+        UpdateOptions();
+    }
+
+    void PopulateOptions()
+    {
+        options.Clear();
+
+        string[] filePaths = Directory.GetFiles(Application.persistentDataPath + "/", "*.dat");
+        for (int x = 0; x < filePaths.Length; x++)
+        {
+            options.Add(filePaths[x].Substring(Application.persistentDataPath.Length + 1, filePaths[x].Length - (Application.persistentDataPath.Length + 5)));
+        }
+    }
+
+    // this is for dropdown moving and selecting stuff. It's still pretty voodoo to me.
+    void Update()
+    {
+        //go to level.
+        if (Input.GetButtonDown("SpinDown"))
+        {
+            if (File.Exists(Application.persistentDataPath + "/" + options[selected] + ".dat"))
+            {
+                GameObject.Find("MrUniverse").GetComponent<TextHolder>().text = (string)options[selected];
+                SceneManager.LoadScene("LevelEditAfterLoad");
+            }
+        }
+
+        //delete all levels
+        if (Input.GetButtonDown("SpinUp"))
+        {
+            options.Clear();
+            string[] filePaths = Directory.GetFiles(Application.persistentDataPath + "/", "*.dat");
+            for (int x = 0; x < filePaths.Length; x++)
+            {
+                File.Delete(filePaths[x]);
+            }
+            UpdateOptions();
+        }
+
+        //goes to main menu
+        if (Input.GetButtonDown("SpinRight"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        //gonna delete something soon.
+        if (Input.GetButtonDown("SpinLeft"))
+        {
+            string[] filePaths = Directory.GetFiles(Application.persistentDataPath + "/", "*.dat");
+
+            File.Delete(filePaths[selected]);
+            PopulateOptions();
+            UpdateOptions();
+        }
+        UpdateSelector();
+
+
+        //timer so selection doesnt't go 15 times every slight touch of the arrow.
+        time += Time.deltaTime;
+        if (time > .1f)
+        {
+            time = 0f;
+
+            selected -= (int)Input.GetAxis("DpadDown");
+            if (selected < 0)
+                selected += options.Count;
+            selected %= options.Count;
+
+        }
+    }
+
+    //update selecing thingy.
+    void UpdateSelector()
+    {
+        selector.text = "";
+        for (int x = 0; x < selected; x++)
+        {
+            selector.text += "\n";
+        }
+        selector.text += "+";
+        selectedItem.text = (string)options[selected];
+    }
+
+    //updates UI list
+    void UpdateOptions()
+    {
+        itemList.text = "";
+        for (int x = 0; x < options.Count; x++)
+        {
+            itemList.text += "   " + options[x] + "\n";
+        }
+    }
+}
