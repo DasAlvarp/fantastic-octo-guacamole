c7ad8ff349d72929c241a3f6918fae0fe1775972
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-05-27 13:59:55 -0400

    Tryting to get visible character, fixed up menu UI

---
+++ Assets/CharacterAnimator.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    public Image wheel;
+    public Image body;
+    public Image head;
+    public Image booster;
+
+    public bool jumping;
+    public bool walking;
+    float angle = 0;
+    float dAngle = .3f;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        jumping = false; ;
+        walking = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Jump();
+        Walk();
+	}
+
+
+
+    void Jump()
+    {
+        if(!jumping)
+        {
+            booster.transform.localPosition =  new Vector3(0,20,0);
+        }
+        else
+        {
+            booster.transform.localPosition = new Vector3(0, 0, 0);
+        }
+    }
+
+    void Walk()
+    {
+        if (angle > 5)
+        {
+            dAngle = -.3f;
+        }
+        else if (angle < -5)
+        {
+            dAngle = .3f;
+        }
+        angle += dAngle;
+        head.transform.Rotate(0,0, dAngle);
+    }
+}
