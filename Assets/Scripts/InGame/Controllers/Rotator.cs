8849854b785780230d859dde3876f02415fc42e7
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-05-30 14:31:02 -0400

    SICK transitions

--- Assets/Scripts/InGame/Controllers/Rotator.cs
+++ Assets/Scripts/InGame/Controllers/Rotator.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
+using System.Collections;
 
-public class Rotator : MonoBehaviour {
+public class Rotator : MonoBehaviour
+{
 
     public GameObject thisGuy;
     public AudioClip rotate;
+    float time = 1f;
+    Vector3 rotateTo = new Vector3(0, 0, 0);
     GameObject player;
     AudioSource sound;
+    Transform pos;
 
 	// Use this for initialization
 	void Start () {
@@ -15,9 +20,10 @@ public class Rotator : MonoBehaviour {
     }
 	
 	// this works better now I guess?
-	void FixedUpdate () {
+	void FixedUpdate ()
+    {
         player = GameObject.Find("Character(Clone)");
-
+        pos = transform;
         Spin();
     }
 
@@ -25,57 +31,94 @@ public class Rotator : MonoBehaviour {
     //spins stage + children based on controller inputs.
     void Spin()
     {
-        Vector3 rotateTo;
         if (Input.GetButtonDown("SpinLeft"))
         {
             sound.Play();
             rotateTo = new Vector3(0, 90, 0);
-            player.transform.position = FloorEr(player.transform.position);
+            StartCoroutine(Rotate(rotateTo, time));
         }
         else if (Input.GetButtonDown("SpinRight"))
         {
             sound.Play();
             rotateTo = new Vector3(0, -90, 0);
-            player.transform.position = FloorEr(player.transform.position);
-
+            StartCoroutine(Rotate(rotateTo, time));
         }
         else if (Input.GetButtonDown("SpinUp"))
         {
             sound.Play();
             rotateTo = new Vector3(-90, 0, 0);
-            player.transform.position = FloorEr(player.transform.position);
+            StartCoroutine(Rotate(rotateTo, time));
+
 
         }
         else if (Input.GetButtonDown("SpinDown"))
         {
             sound.Play();
             rotateTo = new Vector3(90, 0, 0);
-            player.transform.position = FloorEr(player.transform.position);
+            StartCoroutine(Rotate(rotateTo, time));
+
 
         }
         else if(Input.GetButtonDown("RotateRight"))
         {
             sound.Play();
             rotateTo = new Vector3(0, 0, 90);
-            player.transform.position = FloorEr(player.transform.position);
+            StartCoroutine(Rotate(rotateTo, time));
+
 
         }
         else if (Input.GetButtonDown("RotateLeft"))
         {
             sound.Play();
             rotateTo = new Vector3(0, 0, -90);
-            player.transform.position = FloorEr(player.transform.position);
+            StartCoroutine(Rotate(rotateTo, time));
+
 
         }
         else
         {
             rotateTo = new Vector3(0, 0, 0);
         }
-        thisGuy.transform.Rotate(rotateTo, Space.World);
+
+
     }
 
     Vector3 FloorEr(Vector3 vec)
     {
         return new Vector3(vec.x, vec.y,vec.z);//still testing ways to make stuff better. EZ changes.
+    }
+
+    IEnumerator Rotate(Vector3 vec, float time)
+    {
+        print(time);
+        
+        thisGuy.transform.Rotate(vec * Time.deltaTime / time, Space.World);
+        yield return null;
+
+        time -= Time.deltaTime;
+        vec -= vec * Time.deltaTime / time;
+        if (time > 0)
+        {
+            StartCoroutine(Rotate(vec, time));
+        }
+        if (time <= 0)
+        {
+            thisGuy.transform.eulerAngles = RoundTo90(thisGuy.transform);
+            yield return null;
+
+        }
+
+
+    }
+
+    Vector3 RoundTo90(Transform rotation)
+    {
+        Vector3 vec = rotation.eulerAngles;
+        vec.x = Mathf.Round(vec.x / 90) * 90;
+        vec.y = Mathf.Round(vec.y / 90) * 90;
+        vec.z = Mathf.Round(vec.z / 90) * 90;
+        return vec;
+
+
     }
 }
