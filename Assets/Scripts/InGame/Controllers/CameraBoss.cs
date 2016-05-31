9357dac4fa4f4882d8c559e82c80d867ca0b3091
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-05-31 14:01:36 -0400

    these shadows look sick,
    Copied AddThing from unity forums

--- Assets/Scripts/InGame/Controllers/CameraBoss.cs
+++ Assets/Scripts/InGame/Controllers/CameraBoss.cs
@@ -16,7 +16,7 @@ public class CameraBoss : MonoBehaviour
             init = true;
         }
 
-        transform.position = new Vector3(stage.position.x, stage.position.y, character.position.z - character.lossyScale.z / 2 );//idk
+        transform.position = new Vector3(stage.position.x, stage.position.y, character.position.z - character.localScale.z / 2);//idk
         gameObject.GetComponent<Camera>().depth = stage.localScale.z * 10;
         gameObject.GetComponent<Camera>().orthographicSize = GreaterThan(stage.lossyScale.z * 5, (GreaterThan(stage.lossyScale.x * 5, stage.lossyScale.y * 5)));
     }
