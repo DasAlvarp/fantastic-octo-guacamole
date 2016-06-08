92e4597e48dab6f2dccc3bfcb827bdb23b5bd894
Alvaro Gudiswitz-Starred Repositories are significant projects <alvarogudis@gmail.com>
2016-06-08 11:53:25 -0400

    keyboard prompts

---
+++ Assets/Scripts/InGame/TextTogglerJoystick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class TextTogglerJoystick : MonoBehaviour
+{
+
+    public Transform controller;
+    public Transform keyboard;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetJoystickNames()[0] != "")
+        {
+            controller.gameObject.SetActive(true);
+            keyboard.gameObject.SetActive(false);
+        }
+        else
+        {
+            controller.gameObject.SetActive(false);
+            keyboard.gameObject.SetActive(true);
+        }
+    }
+}
